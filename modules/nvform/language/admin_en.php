885c08619423cf20f733d931f804908e14420f47
Nguyễn Nam <nguyennam@vinades.vn>
2023-09-20 10:15:55 +0700

    Cập nhật tiếng Anh cho module nvform

---
+++ modules/nvform/language/admin_en.php
@@ -0,0 +1,211 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Tue, 08 Apr 2014 15:13:43 GMT
+ */
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'VINADES.,JSC (contact@vinades.vn)';
+$lang_translator['createdate'] = '08/04/2014, 15:13';
+$lang_translator['copyright'] = '@Copyright (C) 2014 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['main'] = 'Main page';
+$lang_module['link'] = 'Link';
+$lang_module['copied'] = 'Copied';
+$lang_module['embed'] = 'Embed code';
+$lang_module['config'] = 'Configuration';
+$lang_module['form_info'] = 'Form information';
+$lang_module['form_list'] = 'Form list';
+$lang_module['form_add'] = 'Add form';
+$lang_module['form_content'] = 'Manage forms';
+$lang_module['form_edit'] = 'Edit form';
+$lang_module['form_active'] = 'Active';
+$lang_module['form_deactive'] = 'Inactive';
+$lang_module['form_review'] = 'Preview';
+$lang_module['form_title'] = 'Title';
+$lang_module['form_alias'] = 'Static link';
+$lang_module['form_description'] = 'Description';
+$lang_module['form_description_html'] = 'Detailed content';
+$lang_module['form_image'] = 'Image';
+$lang_module['form_who_view'] = 'View permission';
+$lang_module['form_groups'] = 'If "Member Groups" is selected, select the group from the list below';
+$lang_module['form_active'] = 'Active';
+$lang_module['form_start_time'] = 'From date';
+$lang_module['form_end_time'] = 'Until date';
+$lang_module['form_report'] = 'Statistics';
+$lang_module['form_question_display'] = 'Question display style';
+$lang_module['form_question_display_top'] = 'Question is on top';
+$lang_module['form_question_display_left'] = 'Question is on the left';
+$lang_module['form_question_display_two_column'] = 'Two columns';
+$lang_module['form_question_report'] = 'Results made public';
+$lang_module['form_question_report_note'] = 'Show a link to the results statistics page after the user completes the form';
+$lang_module['form_user_editable'] = 'Change answer';
+$lang_module['form_user_editable_note'] = 'Allow users to change their answers within the validity period';
+$lang_module['form_user_editable_form'] = 'According to form configuration';
+$lang_module['form_report_type'] = 'Statistical form';
+$lang_module['form_report_type_all'] = 'Admin Page and Email';
+$lang_module['form_report_type_acp'] = 'Admin Page Only';
+$lang_module['form_report_type_email'] = 'Email Only';
+$lang_module['form_report_type_email_groups'] = 'Select member groups to receive reports';
+$lang_module['form_report_type_email_maillist'] = 'or enter report email list (Separated by semicolon)';
+$lang_module['form_report_type_email_maillist_note'] = 'Enter email list to receive notifications';
+
+$lang_module['form_template'] = 'Form theme';
+$lang_module['form_template_background'] = 'Page background';
+$lang_module['form_template_background_color'] = 'Background color';
+$lang_module['form_template_background_image'] = 'Image path';
+$lang_module['form_template_background_image_clear'] = 'Clear image';
+$lang_module['form_template_background_image_chosen'] = 'Choose image';
+$lang_module['form_template_background_image_repeat'] = 'Image repeat direction';
+$lang_module['form_template_background_image_repeat_repeat'] = 'Vertical and horizontal';
+$lang_module['form_template_background_image_repeat_y'] = 'Vertical';
+$lang_module['form_template_background_image_repeat_x'] = 'Horizontal';
+$lang_module['form_template_background_image_no_repeat'] = 'No repetition';
+$lang_module['form_template_background_image_position'] = 'Image position';
+$lang_module['form_template_background_image_position_left_top'] = 'Left - Top';
+$lang_module['form_template_background_image_position_left_center'] = 'Left - Center';
+$lang_module['form_template_background_image_position_left_bottom'] = 'Left - Bottom';
+$lang_module['form_template_background_image_position_right_top'] = 'Right - Top';
+$lang_module['form_template_background_image_position_right_center'] = 'Right - Center';
+$lang_module['form_template_background_image_position_right_bottom'] = 'Right - Bottom';
+$lang_module['form_template_background_image_position_center_top'] = 'Middle - Top';
+$lang_module['form_template_background_image_position_center_center'] = 'Center - Center';
+$lang_module['form_template_background_image_position_center_bottom'] = 'Center - Bottom';
+$lang_module['form_export_handler'] = 'Result exporter';
+$lang_module['form_export_handler1'] = 'Customize off-site survey results form';
+
+$lang_module['report_who_answer'] = 'Who answered';
+$lang_module['report_guest'] = 'Guest';
+$lang_module['report_answer_time'] = 'Reply time';
+$lang_module['report_answer_edit_time'] = 'Last updated';
+$lang_module['report_page_title'] = 'List of responses of form "%s"';
+$lang_module['report_count'] = 'There are <strong>%s</strong> respondents';
+$lang_module['report_chart'] = 'View summary';
+$lang_module['report_ex'] = 'Export data';
+$lang_module['report_ex_excel'] = 'Export MS Excel';
+$lang_module['report_ex_calc'] = 'Export Libreoffice Calc';
+$lang_module['report_ex_pdf'] = 'Export PDF';
+$lang_module['report_ex_online'] = 'Open spreadsheet online';
+$lang_module['report_required_phpexcel_title'] = 'PhpSpreadsheet library not found';
+$lang_module['report_required_phpexcel_content'] = 'To use this function you need to install the PhpSpreadsheet library';
+$lang_module['report_type'] = "File format";
+$lang_module['report_zip'] = "Compress files";
+$lang_module['report_zip_note'] = "Contribute file as compressed file (.zip)";
+$lang_module['report_download'] = "Download";
+$lang_module['report_viewtable'] = "View table";
+$lang_module['report_viewpage'] = "View this person's responses";
+$lang_module['report_print'] = "Print page";
+$lang_module['report_all_field'] = "All";
+$lang_module['report_export'] = 'Export form';
+
+$lang_module['question'] = 'Question';
+$lang_module['question_list'] = 'Question list';
+$lang_module['question_manage'] = 'Manage questions';
+$lang_module['question_content'] = 'Question content';
+$lang_module['question_add'] = 'Add question';
+$lang_module['question_edit'] = 'Edit question';
+$lang_module['question_form'] = 'Belongs to form';
+$lang_module['question_required'] = 'Required question';
+$lang_module['question_required_note'] = 'User is required to answer this question';
+$lang_module['question_user_edit'] = 'Allow answer changes';
+$lang_module['question_break'] = 'Page break';
+$lang_module['question_break_note'] = 'Switch to a new page starting from this question';
+$lang_module['question_report'] = '<span class="red">NOT</span> shown in report';
+$lang_module['question_report_note'] = 'If selected, the results of this question will not be displayed in the report';
+$lang_module['question_type'] = 'Data type';
+$lang_module['question_type_number'] = 'Number';
+$lang_module['question_type_date'] = 'Date';
+$lang_module['question_type_time'] = 'Time';
+$lang_module['question_type_textbox'] = 'One line (textbox)';
+$lang_module['question_type_textarea'] = 'Multiple lines (textarea)';
+$lang_module['question_type_editor'] = 'Editor';
+$lang_module['question_type_select'] = 'Dropdown selection (selectbox)';
+$lang_module['question_type_radio'] = 'One option (radio)';
+$lang_module['question_type_checkbox'] = 'Multiple choice (checkbox)';
+$lang_module['question_type_multiselect'] = 'Multi selectbox';
+$lang_module['question_type_grid'] = 'Data grid (select 1 on grid)';
+$lang_module['question_type_grid_row'] = 'Data grid (select 1 on row)';
+$lang_module['question_type_table'] = 'Data table';
+$lang_module['question_type_file'] = 'Attachment';
+$lang_module['question_type_plaintext'] = 'Plain text';
+$lang_module['question_type_plaintext_content'] = 'Text content';
+$lang_module['question_editor_size'] = 'Input box size';
+$lang_module['question_editor_mode'] = 'Editor type';
+$lang_module['question_editor_mode_basic'] = 'Basic editor';
+$lang_module['question_editor_mode_advance'] = 'Abundance editor';
+$lang_module['question_options_text'] = 'Other options';
+$lang_module['question_match_type'] = 'Question to test with value:';
+$lang_module['question_match_type_none'] = 'Do not test';
+$lang_module['question_match_type_alphanumeric'] = 'Only the characters A-Z, 0-9 and underscore can be used';
+$lang_module['question_match_type_url'] = 'Url';
+$lang_module['question_match_type_regex'] = 'Rule expression';
+$lang_module['question_match_type_callback'] = 'Function usage';
+$lang_module['question_default_value'] = 'Default value';
+$lang_module['question_min_length'] = 'Minimum character length';
+$lang_module['question_max_length'] = 'Maximum character length';
+$lang_module['question_min_value'] = 'Minimum value';
+$lang_module['question_max_value'] = 'Maximum value';
+$lang_module['question_options_number'] = 'Data options';
+$lang_module['question_number_type'] = 'Number type';
+$lang_module['question_integer'] = 'Integer';
+$lang_module['question_real'] = 'Real number';
+$lang_module['question_css'] = 'CSS Class';
+
+// Date field
+$lang_module['question_options_date'] = 'Date data options';
+$lang_module['question_current_date'] = 'Use current date';
+$lang_module['question_default_date'] = 'Use date';
+$lang_module['question_empty'] = 'Not in use';
+$lang_module['question_min_date'] = 'From date';
+$lang_module['question_max_date'] = 'Up to date';
+
+// Time field
+$lang_module['question_options_time'] = 'Time data options';
+$lang_module['question_current_time'] = 'Use current time';
+$lang_module['question_default_time'] = 'Specify time';
+
+// Grid field
+$lang_module['question_options_grid'] = 'Grid data options';
+$lang_module['question_options_grid_col'] = 'Data column';
+$lang_module['question_options_grid_row'] = 'Data row';
+
+// File field
+$lang_module['question_options_file'] = 'Upload configuration';
+$lang_module['question_options_file_max_size'] = 'Maximum capacity';
+$lang_module['question_options_file_allow'] = 'Types of files allowed';
+$lang_module['question_options_file_ext_ban'] = 'Extensions are prohibited';
+$lang_module['question_options_file_download'] = 'Download attachment';
+
+$lang_module['question_options_choice'] = 'Options';
+$lang_module['question_choices_empty'] = 'Empty choice values';
+$lang_module['question_number'] = 'STT';
+$lang_module['question_value'] = 'Value';
+$lang_module['question_text'] = 'Description';
+$lang_module['question_add_choice'] = 'Add choice';
+$lang_module['question_add_choice_extend'] = 'Add extended answer';
+$lang_module['question_add_col'] = 'Add column';
+$lang_module['question_add_row'] = 'Add row';
+$lang_module['question_date_error'] = 'The value of Min Date needs to be less than Max Date';
+$lang_module['question_number_error'] = 'Min Value needs to be less than Max Value';
+$lang_module['question_match_type_error'] = '%s does not match the rule';
+$lang_module['question_match_type_required'] = '%s required';
+$lang_module['question_min_max_error'] = '%1$s needs to enter between %2$s and %3$s characters';
+$lang_module['question_min_max_value'] = '%1$s needs to enter from %2$s to %3$s';
+$lang_module['page'] = 'Page';
+$lang_module['order'] = 'Order';
+$lang_module['status'] = 'Status';
+$lang_module['answer'] = 'Answer';
+
+$lang_module['error_groups_choice'] = 'Select at least one group of members';
+$lang_module['error_formtitle'] = 'Enter form title';
+$lang_module['error_formtime'] = 'Start time must be before end time';
+$lang_module['error_save'] = 'An error occurred while saving data';
---
+++ modules/nvform/language/en.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC (contact@vinades.vn)
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Tue, 08 Apr 2014 15:13:43 GMT
+ */
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'VINADES.,JSC (contact@vinades.vn)';
+$lang_translator['createdate'] = '08/04/2014, 15:13';
+$lang_translator['copyright'] = '@Copyright (C) 2014 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['error_savefile'] = 'Error saving file on server';
+$lang_module['error'] = 'Error';
+$lang_module['success'] = 'Completed';
+$lang_module['success_info'] = 'Your response information has been recorded by the system.';
+$lang_module['success_user_info'] = 'You can review the information and export the answer results at <a href="%s">here</a>';
+$lang_module['success_user_info_report'] = 'You can review the information, export the answer results at <a href="%s" title="Review answer">here</a> or view the report general at <a href="%s" title="View report">here</a>';
+$lang_module['form_close_info'] = 'This form will be closed on date %s';
+$lang_module['reset'] = 'Reset';
+$lang_module['next'] = 'Continue';
+$lang_module['prev'] = 'Return';
+$lang_module['status'] = 'Status';
+$lang_module['time_apply'] = 'Validity period';
+$lang_module['answer_count'] = 'Number of replies';
+$lang_module['reply'] = 'Reply';
+$lang_module['answer'] = 'Answer';
+$lang_module['question'] = 'Question';
+$lang_module['export'] = 'Export results';
+
+$lang_module['report_who_answer'] = 'Who answered';
+$lang_module['report_answer_time'] = 'Reply time';
+$lang_module['report_answer_edit_time'] = 'Last updated';
+
+$lang_module['error_form_not_found_detail'] = 'The system did not find any form according to your request.<br />Please contact the administrator for more information.';
+$lang_module['error_form_not_premission_detail'] = 'You are not authorized to access this form.<br />Please contact admin for more details.';
+$lang_module['error_form_not_status_detail'] = 'This form is not activated.<br />Please contact admin for more information.';
+$lang_module['error_form_not_start'] = 'This form will be opened after time: <strong>%s</strong>';
+$lang_module['error_form_closed'] = 'This form closed at <strong>%s</strong>';
+
+$lang_module['field_no_edit'] = '%s is not allowed to be changed';
+$lang_module['field_match_type_error'] = '%s is illegal';
+$lang_module['field_min_max_value'] = '%1$s needs to enter from %2$s to %3$s';
+$lang_module['field_min_max_error'] = '%1$s needs to enter between %2$s and %3$s characters';
+$lang_module['field_match_type_required'] = '%s required';
+$lang_module['field_file_required'] = '%s file not selected';
+$lang_module['field_upload_ext_note'] = 'Allowed file types';
